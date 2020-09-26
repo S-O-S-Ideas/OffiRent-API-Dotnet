d5c51a6e09cbe59410e8d282f0b926d455578fbf
Sentra <54834887+Sentra@users.noreply.github.com>
2020-09-26 18:43:39 -0500

    Update CurrencyResource.cs

--- OffiRent.API/Resources/CurrencyResource.cs
+++ OffiRent.API/Resources/CurrencyResource.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Supermarket.API.Resources
+namespace OffiRent.API.Resources
 {
     public class CurrencyResource
     {
