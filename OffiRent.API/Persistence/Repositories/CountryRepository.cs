80940c82e97b15bc1db09b0205481e36450fdb2b
Sentra <54834887+Sentra@users.noreply.github.com>
2020-09-26 18:40:47 -0500

    Update CountryRepository.cs

--- OffiRent.API/Persistence/Repositories/CountryRepository.cs
+++ OffiRent.API/Persistence/Repositories/CountryRepository.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Supermarket.API.Domain.Models;
-using Supermarket.API.Domain.Persistence.Contexts;
-using Supermarket.API.Domain.Repositories;
+using OffiRent.API.Domain.Models;
+using OffiRent.API.Domain.Persistence.Contexts;
+using OffiRent.API.Domain.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Supermarket.API.Persistence.Repositories
+namespace OffiRent.API.Persistence.Repositories
 {
     public class CountryRepository : BaseRepository, ICountryRepository
     {
