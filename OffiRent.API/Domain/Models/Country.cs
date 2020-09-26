047c569f5342b850ce50a32f14b9ddc05cf71e08
Sentra <54834887+Sentra@users.noreply.github.com>
2020-09-26 18:19:28 -0500

    Update Country.cs

--- OffiRent.API/Domain/Models/Country.cs
+++ OffiRent.API/Domain/Models/Country.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Supermarket.API.Domain.Models
+namespace OffiRent.API.Domain.Models
 {
     public class Country
     {
         public int CountryId { get; set; }
         public char Name { get; set; }
 
-        public List<CountryCurrency> CountryCurrencies { get; set; }
+        public List<CountryCurrency> CountryCurrencies { get; set; } = new List<CountryCurrency>();
 
-        public List<Departament> Departaments { get; set; }
+        public List<Departament> Departaments { get; set; } = new List<Departament> ();
     }
 }
