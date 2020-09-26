b4a5d68ce58b7ca6ced1946c1f772e8da59fc8c5
Sentra <54834887+Sentra@users.noreply.github.com>
2020-09-26 18:47:58 -0500

    Update CurrencyService.cs

--- OffiRent.API/Services/CurrencyService.cs
+++ OffiRent.API/Services/CurrencyService.cs
@@ -1,13 +1,13 @@
-﻿using Supermarket.API.Domain.Models;
-using Supermarket.API.Domain.Repositories;
-using Supermarket.API.Domain.Services;
-using Supermarket.API.Domain.Services.Communications;
+﻿using OffiRent.API.Domain.Models;
+using OffiRent.API.Domain.Repositories;
+using OffiRent.API.Domain.Services;
+using OffiRent.API.Domain.Services.Communications;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Supermarket.API.Services
+namespace OffiRent.API.Services
 {
     public class CurrencyService : ICurrencyService
     {
