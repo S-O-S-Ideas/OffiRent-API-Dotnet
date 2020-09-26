f620deaf3f6b65d27205e5dc91389147c36adaf5
Sentra <54834887+Sentra@users.noreply.github.com>
2020-09-26 18:20:25 -0500

    Update CountryCurrency.cs

--- OffiRent.API/Domain/Models/CountryCurrency.cs
+++ OffiRent.API/Domain/Models/CountryCurrency.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Supermarket.API.Domain.Models
+namespace OffiRent.API.Domain.Models
 {
     public class CountryCurrency
     {
