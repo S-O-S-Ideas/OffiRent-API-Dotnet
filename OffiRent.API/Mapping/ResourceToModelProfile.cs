2a06c6b369a970092d4654d18f94fb0702a531cc
zaj-e <carbonelmarcelo@gmail.com>
2020-09-28 15:01:21 -0500

    mapping fix

--- OffiRent.API/Mapping/ResourceToModelProfile.cs
+++ OffiRent.API/Mapping/ResourceToModelProfile.cs
@@ -2,37 +2,28 @@
 using AutoMapper;
 using OffiRent.API.Domain.Models;
 using OffiRent.API.Resources;
-<<<<<<< HEAD
-=======
 
->>>>>>> a92fdd7fc6f25a560078d3cb7b4e5cadda3a5ff7
 using OffiRent.API.Resources.Account;
 
 
 
-<<<<<<< HEAD
-=======
 
-
->>>>>>> a92fdd7fc6f25a560078d3cb7b4e5cadda3a5ff7
 namespace OffiRent.API.Mapping
 {
     public class ResourceToModelProfile : Profile
     {
         public ResourceToModelProfile()
         {
-<<<<<<< HEAD
+
             CreateMap <SaveAccountResource, AccountResource>();
             CreateMap<SaveCountryResource, CountryResource>();
             CreateMap<SaveCurrencyResource, CurrencyResource>();
-=======
 
             CreateMap <SaveAccountResource, AccountResource>();
 
             CreateMap<SaveCountryResource, CountryResource>();
             CreateMap<SaveCurrencyResource, CurrencyResource>();
 
->>>>>>> a92fdd7fc6f25a560078d3cb7b4e5cadda3a5ff7
         }
     }
 }
